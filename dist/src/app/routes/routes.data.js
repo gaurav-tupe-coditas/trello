0dbbf80cc5b85176fa1ca3eac78515ad8e72f106
Gaurav Tupe <gaurav.tupe@coditas.com>
2026-06-10 17:56:55 +0530

    adding dist to ignore

--- dist/src/app/routes/routes.data.js
+++
@@ -1,3 +0,0 @@
-export const ROUTES = [];
-
-//# sourceMappingURL=routes.data.js.map
--- dist/src/index.js
+++
@@ -1,4 +0,0 @@
-const appModule = await import("./app/app.js");
-appModule.StartServer();
-
-//# sourceMappingURL=index.js.map
